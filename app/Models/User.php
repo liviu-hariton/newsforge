d9727dc31dcd27a606a32816b5cc759809915455
Liviu Hariton <liviu.hariton@gmail.com>
2024-06-21 17:42:29 +0300

    Resume work on project

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Mail;
